b5ee21204026a6f5d0af3410c3d83575d5b1a36e
Jordan Russell <jorussell@ptschools.org>
2026-02-24 11:27:35 -0800

    Test samba-usershares
    I was basically reading the aurora stuff and I found this thing which
    looks cool and I want to see how it works
    
    https://github.com/ublue-os/aurora/blob/main/build_files/base/16-override-install.sh#L67
    new file:   files/scripts/02-samba-usershares.sh: New script to setup samba-usershares
    modified:   recipes/common-modules.yml: Add samba-usershares

---
+++ files/scripts/02-samba-usershares.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+mkdir -p /var/lib/samba/usershares
+chown -R root:usershares /var/lib/samba/usershares
+firewall-offline-cmd --service=samba --service=samba-client
+setsebool -P samba_enable_home_dirs=1
+setsebool -P samba_export_all_ro=1
+setsebool -P samba_export_all_rw=1
+sed -i '/^\[homes\]/,/^\[/{/^\[homes\]/d;/^\[/!d}' /etc/samba/smb.conf
